704ebf371df31a4440986510f30d167ebca417f4
sasirekha <ssnarayanan85@gmail.com>
2021-11-06 23:20:01 -0500

    New file upload

---
+++ firstFile.py
@@ -0,0 +1,12 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+        .master("local") \
+        .appName("firstfile") \
+        .getOrCreate()
+
+data = [('k1', 'v1'), ('k2','v2'), ('k3','v')]
+columns = ("key", "val")
+df = spark.createDataFrame(data, columns)
+df.show()
+df.printSchema()
