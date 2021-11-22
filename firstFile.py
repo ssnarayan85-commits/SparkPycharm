ce3b381d814d568cb716a81f988747b836870175
sasirekha <ssnarayanan85@gmail.com>
2021-11-22 00:20:56 -0600

    added a new module pytest

--- firstFile.py
+++ firstFile.py
@@ -1,4 +1,7 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import concat,lit
+import pyspark.sql.functions as F
+import pytest # added a new module
 
 #spark session
 spark = SparkSession.builder\
@@ -6,8 +9,15 @@ spark = SparkSession.builder\
         .appName("firstfile") \
         .getOrCreate()
 
-data = [('k1', 'v1'), ('k2','v2'), ('k3','v')]
-columns = ("key", "val")
+# def add_fullname_udf(firstname, lastname):
+#         print( concat(firstname, " ",  lastname))
+
+data = [('sasi', 'rekha', 1000), ('vasantha','kumar',2000), ('shelby','zacharia',3000)]
+columns = ("Firstname", "Lastname", "Salary")
 df = spark.createDataFrame(data, columns)
-df.show()
+
+df.select(df["Firstname"], df["Lastname"], concat(df.Firstname, lit(" "), df.Lastname).alias("Fullname"),\
+          (df.Salary * 0.3).alias("Bonus")).show()
+
 df.printSchema()
+df.show(20)
